a332d8e97227346b65ede71c866f29646ba60f73
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-20 02:39:46 +0300

    fix: eslint fix

--- src/Product/View/Slider.jsx
+++ src/Product/View/Slider.jsx
@@ -32,39 +32,22 @@ const Img = styled.img`
 function SliderImg(props) {
   return (
     <Picture>
-      <source media="(min-width: 48rem)" srcSet={props.srcset} />
-      <source media="(min-width: 62rem)" srcSet={props.srcset} />
+      <source media="(min-width: 48rem)" srcSet="/img/img1.jpg 1x, /img/img1@2x.jpg 2x" />
+      <source media="(min-width: 62rem)" srcSet="/img/img1@2x.jpg 1x, /img/img1@3x.jpg 2x" />
       <Img src={props.src} alt={props.alt} />
     </Picture>
   );
 }
 
 SliderImg.propTypes = {
-  srcset: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 };
 
 export default () =>
   (<Slider>
-    <SliderImg
-      src="/img/img4.jpg"
-      srcset="/img/img4.jpg 1x, /img/img4@2x.jpg 2x, /img/img4@3x.jpg 3x"
-      alt="gabardine coat front img"
-    />
-    <SliderImg
-      src="/img/img3.jpg"
-      srcset="/img/img3.jpg 1x, /img/img3@2x.jpg 2x, /img/img3@3x.jpg 3x"
-      alt="gabardine coat img 2"
-    />
-    <SliderImg
-      src="/img/img2.jpg"
-      srcset="/img/img2.jpg 1x, /img/img2@2x.jpg 2x, /img/img2@3x.jpg 3x"
-      alt="gabardine coat img 3"
-    />
-    <SliderImg
-      src="/img/img1.jpg"
-      srcset="/img/img1.jpg 1x, /img/img1@2x.jpg 2x, /img/img1@3x.jpg 3x"
-      alt="gabardine coat img 4"
-    />
+    <SliderImg src="/img/img4.jpg" alt="gabardine coat front img" />
+    <SliderImg src="/img/img3.jpg" alt="gabardine coat img 2" />
+    <SliderImg src="/img/img2.jpg" alt="gabardine coat img 3" />
+    <SliderImg src="/img/img1.jpg" alt="gabardine coat img 4" />
   </Slider>);
