1069906e1f08b9873506f0295f624319a8b5c4b6
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-20 04:04:53 +0300

    fix: slider fixes

--- src/Product/View/Slider.jsx
+++ src/Product/View/Slider.jsx
@@ -19,35 +19,35 @@ const Slider = styled.div`
 const Picture = styled.picture``;
 
 const Img = styled.img`
-  min-height: 25rem;
+  height: 25rem;
   vertical-align: top;
   @media only screen and (min-width: 48rem) {
-    min-height: 36rem;
+    height: 36rem;
   }
   @media only screen and (min-width: 62rem) {
-    min-height: 40.75rem;
+    height: 40.75rem;
   }
 `;
 
 function SliderImg(props) {
   return (
     <Picture>
-      <source media="(min-width: 48rem)" srcSet="/img/img1.jpg 1x, /img/img1@2x.jpg 2x" />
-      <source media="(min-width: 62rem)" srcSet="/img/img1@2x.jpg 1x, /img/img1@3x.jpg 2x" />
+      <source media="(min-width: 48rem)" srcSet={props.srcset} />
       <Img src={props.src} alt={props.alt} />
     </Picture>
   );
 }
 
 SliderImg.propTypes = {
+  srcset: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 };
 
 export default () =>
   (<Slider>
-    <SliderImg src="/img/img4.jpg" alt="gabardine coat front img" />
-    <SliderImg src="/img/img3.jpg" alt="gabardine coat img 2" />
-    <SliderImg src="/img/img2.jpg" alt="gabardine coat img 3" />
-    <SliderImg src="/img/img1.jpg" alt="gabardine coat img 4" />
+    <SliderImg src="/img/img4.jpg" srcset="/img/img4@2x.jpg" alt="gabardine coat front img" />
+    <SliderImg src="/img/img3.jpg" srcset="/img/img3@2x.jpg" alt="gabardine coat img 2" />
+    <SliderImg src="/img/img2.jpg" srcset="/img/img2@2x.jpg" alt="gabardine coat img 3" />
+    <SliderImg src="/img/img1.jpg" srcset="/img/img1@2x.jpg" alt="gabardine coat img 4" />
   </Slider>);
