4d25fb31e1dac769ab9244f901f52c77b8c20b49
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-28 13:55:32 +0300

    fix: filters dropdowns added and fixed

---
+++ src/Product/List/Toolbar/Filter.jsx
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import chevron from '../../../assets/arrow.svg';
+
+const DropdownWrap = styled.div`
+  position: relative;
+  margin-right: 1rem;
+  @media screen and (min-width: 48rem) {
+    margin-right: 3rem;
+    margin-right: ${props => (props.align === 'right' ? '0' : '3rem')};
+    margin-left: ${props => (props.align === 'right' ? 'auto' : '0')};
+  }
+`;
+
+const Btn = styled.button`
+  display: block;
+  padding: 1.5rem 0;
+  font-family: Raleway, Helvetica Neue, Helvetica, sans-serif;
+  font-size: .75rem;
+  line-height: 1rem;
+  border: none;
+  white-space: nowrap;
+  background: transparent;
+  outline: 0;
+  color: ${props => props.color};
+  color: ${props => (props.isOpened ? '#171717' : '')};
+
+  &:after {
+    content: "";
+    display: inline-block;
+    width: 12px;
+    height: 6px;
+    margin-left: .5rem;
+    background-image: url(${chevron});
+    background-size: cover;
+    transform: ${props => (props.isOpened ? 'rotate(-180deg)' : 'none')};
+  }
+`;
+
+const DropdownWindow = styled.div`
+  position: absolute;
+  left: -.5rem;
+  top: 100%;
+  display: inline-block;
+  width: 377px;
+  padding: .5rem;
+  padding-top: 1rem;
+  background-color: #f3f3f3;
+  font-size: .75rem;
+  line-height: 1rem;
+
+  @media screen and (min-width: 48rem) {
+    padding: 1.5rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    left: ${props => (props.align === 'right' ? 'auto' : '-1.5rem')};
+    right: ${props => (props.align === 'right' ? '-1.5rem' : 'auto')};
+    width: ${props => (props.align === 'right' ? '149px' : '377px')};
+  }
+`;
+
+function Button(props) {
+  return (
+    <Btn onClick={props.onClick} isOpened={props.isOpened} color={props.color}>
+      {props.value}
+    </Btn>
+  );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  isOpened: PropTypes.bool.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    this.state = {
+      isOpened: false,
+    };
+  }
+
+  handleClick() {
+    if (!this.state.isOpened) {
+      document.addEventListener('click', this.handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false);
+    }
+
+    this.setState(prevState => ({
+      isOpened: !prevState.isOpened,
+    }));
+  }
+
+  handleOutsideClick(e) {
+    if (this.node.parentNode === e.target) {
+      return;
+    }
+
+    this.handleClick();
+  }
+
+  render() {
+    return (
+      <DropdownWrap
+        align={this.props.align}
+        ref={(node) => {
+          this.node = node;
+        }}
+      >
+        <Button
+          onClick={() => {
+            this.handleClick();
+            this.props.action();
+          }}
+          value={this.props.value}
+          isOpened={this.state.isOpened}
+          color={this.props.color}
+        />
+        {this.state.isOpened &&
+          <DropdownWindow align={this.props.align}>
+            {this.props.children}
+          </DropdownWindow>}
+      </DropdownWrap>
+    );
+  }
+}
+
+Dropdown.propTypes = {
+  children: PropTypes.node.isRequired,
+  value: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  align: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+};
+
+export default Dropdown;
--- src/Product/List/Toolbar/Filters.jsx
+++ src/Product/List/Toolbar/Filters.jsx
@@ -1,119 +1,60 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import chevron from '../../../assets/arrow.svg';
+import Dropdown from './Filter';
 
 const Wrapper = styled.div`display: flex;`;
 
-const Btn = styled.button`
-  display: block;
-  padding: 1.5rem 0;
-  font-family: Raleway, Helvetica Neue, Helvetica, sans-serif;
-  font-size: .75rem;
-  line-height: 1rem;
-  border: none;
-  white-space: nowrap;
-  background: transparent;
-  outline: 0;
-
-  &:after {
-    content: "";
-    display: inline-block;
-    width: 12px;
-    height: 6px;
-    margin-left: .5rem;
-    background-image: url(${chevron});
-    background-size: cover;
-  }
-`;
-
-function Button(props) {
-  return (
-    <Btn onClick={props.onClick}>
-      {props.value}
-    </Btn>
-  );
-}
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
-};
-
-const DropdownWrap = styled.div`
-  position: relative;
-  margin-right: 1rem;
-  @media screen and (min-width: 48rem) {
-    margin-right: 3rem;
-
-    &:last-child {
-      margin: 0;
-      margin-left: auto;
-    }
-  }
-`;
-
-// const DropdownWindow = styled.div`
-//   position: absolute;
-//   left: -1.5rem;
-//   top: 100%;
-//   display: inline-block;
-//   width: 377px;
-//   padding: 1.5rem;
-//   padding-top: 1rem;
-//   background-color: #f3f3f3;
-// `;
-
-class Dropdown extends Component {
+class Toolbar extends Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
     this.state = {
-      isOpened: false,
+      color: '#171717',
     };
+    this.onDropdown = this.onDropdown.bind(this);
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      isOpened: !prevState.isOpened,
-    }));
+  onDropdown() {
+    this.setState(this.state.color === '#171717' ? { color: '#999' } : { color: '#171717' });
   }
 
   render() {
     return (
-      <DropdownWrap>
-        <Button onClick={this.handleClick} value={this.props.value} />
-        {this.state.isOpened &&
+      <Wrapper>
+        <Dropdown value="Category" color={this.state.color} action={this.onDropdown}>
           <div>
-            {this.props.children}
-          </div>}
-      </DropdownWrap>
+            Content content content content content content content content content content content
+            content content content content content content content content content content content
+            content content content content content content content content content content content
+            content content content content content content content content content
+          </div>
+        </Dropdown>
+        <Dropdown value="Colour" color={this.state.color} action={this.onDropdown}>
+          <div>
+            Content content content content content content content content content content content
+            content content content content content content content content content content content
+            content content content content content content content content content content content
+            content content content content content content content content content
+          </div>
+        </Dropdown>
+        <Dropdown value="Size" color={this.state.color} action={this.onDropdown}>
+          <div>
+            Content content content content content content content content content content content
+            content content content content content content content content content content content
+            content content content content content content content content content content content
+            content content content content content content content content content
+          </div>
+        </Dropdown>
+        <Dropdown
+          align="right"
+          value="Sort by price"
+          color={this.state.color}
+          action={this.onDropdown}
+        >
+          <div>high or low it’s medium length of content</div>
+        </Dropdown>
+      </Wrapper>
     );
   }
-}
-
-Dropdown.propTypes = {
-  children: PropTypes.node.isRequired,
-  value: PropTypes.string.isRequired,
-};
-
-function Toolbar() {
-  return (
-    <Wrapper>
-      <Dropdown value="Category">
-        <div>CONTENT1</div>
-      </Dropdown>
-      <Dropdown value="Colour">
-        <div>CONTENT2</div>
-      </Dropdown>
-      <Dropdown value="Size">
-        <div>CONTENT3</div>
-      </Dropdown>
-      <Dropdown value="Sort by price">
-        <div>CONTENT4</div>
-      </Dropdown>
-    </Wrapper>
-  );
 }
 
 export default Toolbar;
