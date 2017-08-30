55a931f4db466c66aa1c7d92cee3f0717250874e
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-30 22:59:26 +0300

    fix: product category component fixed

--- src/Product/List/Category.jsx
+++ src/Product/List/Category.jsx
@@ -18,113 +18,111 @@ const Heading = styled.h2`
 `;
 
 function Head(props) {
-  return (
-    <Heading>
-      {props.heading}
-    </Heading>
-  );
+  return <Heading>{props.heading}</Heading>;
 }
 
 Head.propTypes = {
   heading: PropTypes.string.isRequired,
 };
 
+const productsCategoryOne = [
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/a2fa084eae958434c326685fc8ff19dfce9fe430.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    alt: 'The Westminster – Long Heritage Trench Coat',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/fb6adea94455f2a73e97b5cf2d7811d9135dcbe2.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    alt: 'The Westminster – Long Heritage Trench Coat',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/90dd344122ccf1884fce63c4fc775bd6baa7a11f.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/995466e7e1113f3b2f6484ceb090072e1c9062dc.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/a2fa084eae958434c326685fc8ff19dfce9fe430.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    alt: 'The Westminster – Long Heritage Trench Coat',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/fb6adea94455f2a73e97b5cf2d7811d9135dcbe2.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    alt: 'The Westminster – Long Heritage Trench Coat',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/90dd344122ccf1884fce63c4fc775bd6baa7a11f.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+  {
+    to: '/men/clothes/trenchcoat',
+    img:
+      'https://assets.burberry.com/is/image/Burberryltd/995466e7e1113f3b2f6484ceb090072e1c9062dc.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505',
+    label: 'Regular fit',
+    name: 'The Westminster – Long Heritage Trench Coat',
+    colorsCount: 3,
+    price: 1395,
+  },
+];
+
 function ProductCategory(props) {
+  const products = productsCategoryOne.map((product, key) => (
+    <div className="col-xs-6 col-md-3">
+      <ProductPreviewExtended
+        key={key.toString()}
+        to={product.to}
+        img={product.img}
+        alt={product.name}
+        label={product.label}
+        name={product.name}
+        colorsCount={product.colorsCount}
+        price={product.price}
+      />
+    </div>
+  ));
   return (
     <div>
       <Head heading={props.heading} />
-      <div className="row">
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/a2fa084eae958434c326685fc8ff19dfce9fe430.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={1395}
-          />
-        </div>
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/fb6adea94455f2a73e97b5cf2d7811d9135dcbe2.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={12500}
-          />
-        </div>
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/90dd344122ccf1884fce63c4fc775bd6baa7a11f.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={27000}
-          />
-        </div>
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/995466e7e1113f3b2f6484ceb090072e1c9062dc.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={11000}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/a2fa084eae958434c326685fc8ff19dfce9fe430.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={1395}
-          />
-        </div>
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/fb6adea94455f2a73e97b5cf2d7811d9135dcbe2.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={12500}
-          />
-        </div>
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/90dd344122ccf1884fce63c4fc775bd6baa7a11f.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={27000}
-          />
-        </div>
-        <div className="col-xs-6 col-md-3">
-          <ProductPreviewExtended
-            to="/men/clothes/trenchcoat"
-            img="https://assets.burberry.com/is/image/Burberryltd/995466e7e1113f3b2f6484ceb090072e1c9062dc.jpg?$BBY_V2_ML_3X4$&wid=378&hei=505"
-            alt="The Westminster – Long Heritage Trench Coat"
-            label="Regular fit"
-            name="The Westminster – Long Heritage Trench Coat"
-            colorsCount={3}
-            price={11000}
-          />
-        </div>
-      </div>
+      <div className="row">{products}</div>
     </div>
   );
 }
--- src/Product/List/index.jsx
+++ src/Product/List/index.jsx
@@ -4,13 +4,14 @@ import ProductCategory from './Category';
 import More from './ShowMore';
 import Divider from '../../common/Divider';
 
-export default () =>
-  (<main>
+export default () => (
+  <main>
     <Toolbar />
     <div className="container">
       <ProductCategory heading="Heritage Trench Coats" />
       <Divider />
       <ProductCategory heading="Single Breasted Trench Coats" />
+      <More viewed={8} summ={17} count={9} />
     </div>
-    <More viewed={8} summ={17} count={9} />
-  </main>);
+  </main>
+);
